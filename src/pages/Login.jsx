46cff872a2e119879d557b163fa24ed30aa86ad5
ANWDM <pv4003337@gmail.com>
2023-04-12 03:28:03 +0530

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Home from '../assets/image/home.png'
+import { Formik } from 'formik'
+
+const Login = () => {
+  return (
+    <div className='flex items-center justify-center'>
+        <div>
+            <img src={Home} className='h-[540px]' />
+        </div>
+        <div>
+            <div>
+                <h1>Instagram</h1>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
